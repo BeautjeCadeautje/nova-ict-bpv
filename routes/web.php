20ae6b7e593bc54ace1e26b5be8e7c45b1c94894
Beau.s <9isa.beau@gmail.com>
2025-05-13 08:53:14 +0200

    more routes

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,13 @@ Route::get('/internships', [InternshipController::class, 'index'])->name('intern
 Route::get('/internships/create', [InternshipController::class, 'create'])->name('internships.create'); // <-- verplaatst
 Route::post('/internships', [ InternshipController::class, 'store' ]);
 Route::get('/internships/{id}', [InternshipController::class, 'show'])->name('internships.show');
+Route::get('/internships/{id}/edit', [ InternshipController::class, 'edit' ]);
+Route::put('/internships/{id}', [ InternshipController::class, 'update' ]);
 
-
+Route::get('/educations/create', [EducationController::class, 'create'])->name('educations.create');
+Route::post('/educations', [EducationController::class, 'store']);
 Route::get('/educations', [EducationController::class, 'index'])->name('educations.index');
 Route::get('/educations/{id}', [EducationController::class, 'show'])->name('educations.show');
+Route::get('/educations/{id}/edit', [EducationController::class, 'edit'])->name('educations.edit');
+Route::put('/educations/{id}', [EducationController::class, 'update'])->name('educations.update');
 
